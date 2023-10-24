07edad3a0fcd9dc8affe41c5ea20047627d3c30a
Thomas Maghanga <142288589+tom-maghanga@users.noreply.github.com>
2023-10-24 11:43:38 +0300

    added students list and url mapping for the students

--- src/main/java/com/springboot/tomdemo/StudentController.java
+++ src/main/java/com/springboot/tomdemo/StudentController.java
@@ -4,6 +4,9 @@ import bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -11,5 +14,13 @@ public class StudentController {
     public Student getStudent( ){
         Student student = new Student(1,"Thomas", "Maghanga");
         return student;
+    }
+
+    @GetMapping("students")
+    public List<Student> getstudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(2, "Jackim", "Mwaviswa"));
+        students.add((new Student(3, "Emmanuel", "Righa")));
+        return  students;
     }
 }
